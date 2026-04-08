8815fa6cb873c3fdef6ce1cf40c31da5c3b88fa2
MariaBarroeta <notaryflservice@gmail.com>
2026-04-07 23:58:23 -0400

    Refactor import statement for RootRoute and add CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RootRoute } from './routes/__root'
+import RootRoute from './routes/__root'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
